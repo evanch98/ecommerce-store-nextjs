1ea0c00bd8d9240485f34d6a95ec286ec19ad644
Kyaw Thu <evanch98@gmail.com>
2023-08-10 00:07:25 +0800

    update the MainNav component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 interface MainNavProps {
@@ -15,7 +18,9 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      Main Nav
+      {routes.map((route) => (
+        <Link key={route.href} href={route.href} className="">{route.label}</Link>
+      ))}
     </nav>
   );
 };
