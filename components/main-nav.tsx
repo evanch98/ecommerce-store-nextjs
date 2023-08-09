ae9a7536178a7f5f7b06136fc53042b38a8b2383
Kyaw Thu <evanch98@gmail.com>
2023-08-10 00:10:49 +0800

    update the MainNav component

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -19,7 +20,16 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
       {routes.map((route) => (
-        <Link key={route.href} href={route.href} className="">{route.label}</Link>
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-black",
+            route.active ? "text-black" : "text-neutral-500"
+          )}
+        >
+          {route.label}
+        </Link>
       ))}
     </nav>
   );
