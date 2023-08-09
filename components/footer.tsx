6f1fa302829c72c87a0cda787a0ad4ae4e8064c6
Kyaw Thu <evanch98@gmail.com>
2023-08-09 23:35:55 +0800

    update the Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 E-commerce Store, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
