757c19fcb2f9c4aa9fc0ec14ef8f62bb3c519da5
Kyaw Thu <evanch98@gmail.com>
2023-08-09 23:38:40 +0800

    update the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/footer";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
+import Navbar from "@/components/navbar";
 
 const font = Urbanist({ subsets: ["latin"] });
 
