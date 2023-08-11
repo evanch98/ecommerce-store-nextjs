45a461dd2d93bbb8d67ea4380ff021bbbebe4656
Kyaw Thu <evanch98@gmail.com>
2023-08-11 14:21:14 +0800

    update the HomePage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,15 @@
+import getBillboard from "@/actions/get-billboard";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0; // to make sure it's never cached
 
-const HomePage = () => {
+const HomePage = async () => {
+  const billboard = await getBillboard("b25f11e2-97c1-443e-92e3-da667ea15753");
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
