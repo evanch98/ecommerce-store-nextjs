c0b1b240d3ab784a44bdffe8a48adeeef944985e
Kyaw Thu <evanch98@gmail.com>
2023-08-11 12:54:18 +0800

    update the Navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,6 +15,7 @@ const Navbar = async () => {
             <p className="font-bold text-xl">STORE</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
