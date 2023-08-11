a0f8676c3e9d94a2a3467af9b851409fff3c83a1
Kyaw Thu <evanch98@gmail.com>
2023-08-11 13:11:08 +0800

    update the NavbarActions component

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import Button from "@/components/ui/button";
+
 const NavbarActions = () => {
   return (
-    <div className="ml-auto flex items-center gap-x-4">Navbar Actions</div>
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button></Button>
+    </div>
   );
 };
 
