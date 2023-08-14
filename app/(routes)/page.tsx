d1d208f808d26e088e775599ea4d465353cac41d
Kyaw Thu <evanch98@gmail.com>
2023-08-14 13:26:36 +0800

    update the HomePage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,7 @@
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0; // to make sure it's never cached
