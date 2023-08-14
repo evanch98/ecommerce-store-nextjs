c3f0612f023f22235e46808c21976aa7532eda53
Kyaw Thu <evanch98@gmail.com>
2023-08-14 13:23:49 +0800

    update the HomePage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
@@ -6,10 +7,14 @@ export const revalidate = 0; // to make sure it's never cached
 
 const HomePage = async () => {
   const billboard = await getBillboard("27fbdcc2-6041-42c6-8245-3605ed68c657");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   );
