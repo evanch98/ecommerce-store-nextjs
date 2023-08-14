57ed218921576753a7e4370b26526fe2c82e035d
Kyaw Thu <evanch98@gmail.com>
2023-08-14 13:26:02 +0800

    add a new component ProductList

---
+++ components/product-list.tsx
@@ -0,0 +1,16 @@
+import { Product } from "@/types";
+
+interface ProductListPros {
+  title: string;
+  items: Product[];
+}
+
+const ProductList: React.FC<ProductListPros> = ({ title, items }) => {
+  return (
+    <div className="space-y-4">
+      <h3>{title}</h3>
+    </div>
+  );
+};
+
+export default ProductList;
