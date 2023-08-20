0514798cceb860dfe88f92367aaa14cc41bb49e6
Kyaw Thu <evanch98@gmail.com>
2023-08-20 16:14:33 +0800

    update the Currency

--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -1,5 +1,14 @@
-const Currency = () => {
-  return <div className="font-semibold"></div>;
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+interface CurrencyProps {
+  value?: string | number;
+}
+
+const Currency: React.FC<CurrencyProps> = ({ value }) => {
+  return <div className="font-semibold">{formatter.format(Number(value))}</div>;
 };
 
 export default Currency;
