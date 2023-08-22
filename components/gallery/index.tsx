1ac98fdcac97405bff2599093b7e8007c6a42ecc
Kyaw Thu <evanch98@gmail.com>
2023-08-22 19:37:01 +0800

    update the Gallery

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Tab } from "@headlessui/react";
 import { Image as ImageType } from "@/types";
+import GalleryTab from "./gallery-tab";
 
 interface GalleryProps {
   images: ImageType[];
