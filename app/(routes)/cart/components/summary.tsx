3fda8b85deb7591094531d695c319f9e70e29cd0
Kyaw Thu <evanch98@gmail.com>
2023-08-23 21:56:23 +0800

    add a new component Summary

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -1,1 +1,15 @@
 "use client";
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import Button from "@/components/ui/button";
+import Currency from "@/components/ui/currency";
+import useCart from "@/hooks/use-cart";
+import { toast } from "react-hot-toast";
+
+const Summary = () => {
+  return <div>Summary</div>;
+};
+
+export default Summary;
