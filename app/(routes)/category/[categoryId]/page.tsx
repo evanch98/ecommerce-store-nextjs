c9753866b3c319f5131617afa4b328bbb224e28b
Kyaw Thu <evanch98@gmail.com>
2023-08-23 12:32:14 +0800

    update the ProductPage

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -34,6 +34,14 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     <div className="bg-white">
       <Container>
         <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            {/* Add Mobile Filters */}
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
