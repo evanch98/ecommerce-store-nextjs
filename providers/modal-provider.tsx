f9417d3a7383b41ad91f3c0c88495211517445d1
Kyaw Thu <evanch98@gmail.com>
2023-08-23 20:56:22 +0800

    update the ModalProvider component

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import PreviewModal from "@/components/preview-modal";
+import { useEffect, useState } from "react";
+
 const ModalProvider = () => {
-  return <></>;
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
 };
 
 export default ModalProvider;
